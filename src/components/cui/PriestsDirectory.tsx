3d7a6f5c3a167274d074f22d3d47e4635b0619a7
stanjae <stanykhay29@gmail.com>
2025-06-27 13:29:18 -0400

    fixed infinite scroll pagination

--- src/components/cui/PriestsDirectory.tsx
+++ src/components/cui/PriestsDirectory.tsx
@@ -1,77 +1,103 @@
-'use client'
-import React, { useEffect } from 'react'
-import {useInView} from 'react-intersection-observer'
-import SpinLoader from './SpinLoader'
-import { useInfiniteQuery } from '@tanstack/react-query'
-import { getPriests } from '@/lib/actions'
-import { PriestDetail } from '@/utils/definitions'
-import { CCard } from './CCard'
-import { useSearchParams } from 'next/navigation'
-
+"use client";
+import React, { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+import SpinLoader from "./SpinLoader";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { getPriests } from "@/lib/actions";
+import { PriestDetail } from "@/utils/definitions";
+import { CCard } from "./CCard";
+import { useSearchParams } from "next/navigation";
+import { Alert, AlertDescription, AlertTitle } from "../ui/alert";
+import { AlertCircleIcon } from "lucide-react";
 
 const PriestsDirectory = () => {
-    const { ref, inView } = useInView()
-    const searchParams = useSearchParams();
+  const { ref, inView } = useInView();
+  const searchParams = useSearchParams();
 
+  const {
+    status,
+    data,
+    error,
+    isFetching,
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+  } = useInfiniteQuery({
+    queryKey: [
+      "priests",
+      searchParams.get("query")?.toString(),
+      searchParams.get("position")?.toString(),
+      searchParams.get("title")?.toString(),
+      searchParams.get("sort")?.toString(),
+    ],
+    queryFn: async ({
+      pageParam,
+    }): Promise<{
+      data: Array<PriestDetail>;
+      previousId: number;
+      nextId: number;
+    }> => {
+      const response = await getPriests(
+        pageParam,
+        searchParams.get("query")?.toString(),
+        searchParams.get("title")?.toString(),
+        searchParams.get("position")?.toString(),
+        searchParams.get("sort")?.toString()
+      );
+      return response;
+    },
+    initialPageParam: 1,
+    getPreviousPageParam: (firstPage) => firstPage.previousId,
+    getNextPageParam: (lastPage) => lastPage.nextId,
+  });
 
-    const {
-        status,
-        data,
-        error,
-        isFetching,
-        isFetchingNextPage,
-        fetchNextPage,
-        hasNextPage,
-      } = useInfiniteQuery({
-        queryKey: ['priests', searchParams.get('query')?.toString(), searchParams.get('position')?.toString(), searchParams.get('title')?.toString(), searchParams.get('sort')?.toString()],
-        queryFn: async ({
-          pageParam,
-        }): Promise<{
-          data: Array<PriestDetail>
-          previousId: number
-          nextId: number
-        }> => {
-          const response = await getPriests(pageParam, searchParams.get('query')?.toString(), searchParams.get('title')?.toString(), searchParams.get('position')?.toString(), searchParams.get('sort')?.toString());
-          return response
-        },
-        initialPageParam: 1,
-        getPreviousPageParam: (firstPage) => firstPage.previousId,
-        getNextPageParam: (lastPage) => lastPage.nextId,
-      })
-
-
-      useEffect(() => {
-        if (inView) {
-          fetchNextPage()
-        }
-      }, [fetchNextPage, inView])
+  useEffect(() => {
+    if (inView) {
+      fetchNextPage();
+    }
+  }, [fetchNextPage, inView]);
 
   return (
-    <section className=' py-10'>
-      <div className='mt-3 relative'>
-          {status === 'pending' ? (
-              <SpinLoader/>
-            ) : status === 'error' ? (
-              <span>Error: {error.message}</span>
-            ):null}
+    <section className=" py-10">
+      <div className="mt-3 relative">
+        {status === "pending" ? (
+          <SpinLoader />
+        ) : status === "error" ? (
+          <code>Error: {error.message}</code>
+        ) : null}
       </div>
 
-        <div className={` max-w-8xl  mx-auto grid gap-10 md:grid-cols-3 lg:grid-cols-5 grid-cols-1 sm:grid-cols-2 `}>
-            {data?.pages?.map((page)=> (page?.data?.map((item)=> (<CCard key={item._id} item={item}/>))))}
-        </div>
+      <div
+        className={` max-w-8xl  mx-auto grid gap-10 md:grid-cols-3 lg:grid-cols-5 grid-cols-1 sm:grid-cols-2 `}
+      >
+        {data?.pages?.map((page) =>
+          page?.data?.map((item) => <CCard key={item._id} item={item} />)
+        )}
+      </div>
 
-
-
-          <div className='relative bg-amber-500' ref={ref}>
-              {isFetchingNextPage || hasNextPage || isFetching
-                ? <SpinLoader/>
-                  : 'Nothing more to load'}
+      <div className="relative my-2 flex" ref={ref}>
+        {!searchParams.get("query")?.toString() &&
+        (isFetchingNextPage || hasNextPage || isFetching) ? (
+          <SpinLoader />
+        ) : searchParams.get("query")?.toString() && data?.pages?.length === 0 ? (
+          <Alert className="mx-auto max-w-3xl text-brown" >
+            <AlertCircleIcon className=" !size-6" />
+            <AlertTitle className=" text-2xl">No Searches found</AlertTitle>
+            <AlertDescription className=" text-lg">
+              Try searching for something else
+            </AlertDescription>
+          </Alert>
+        ) : (
+          <div className=" px-2 py-1 bg-brown text-white rounded-xl italic mx-auto">
+            Nothing more to load
           </div>
+        )}
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default PriestsDirectory
+export default PriestsDirectory;
 
 /* import React from 'react'
 import Link from 'next/link'
